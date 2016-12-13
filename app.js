52c18508a717516a3ed34c71ffdd2b6b453e88e5
Sthefany Floriano <Sthefany Floriano>
2016-12-13 15:59:13 -0500

    add logo

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname + "/src"));
-app.use(express.static(__dirname + "/static"));
-app.listen(3000 , function(){
-  console.log("Servidor Encendido");
-});
